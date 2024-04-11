178ab6a200b6c20a1020b6ae93e4e579cef732de
dinghoi77 <dinghoi77@gmail.com>
2024-04-11 10:49:06 +0900

    add sql query files

---
+++ database/sql/tb_del_all.sql
@@ -0,0 +1,21 @@
+
+# 테이블 전체 삭제 쿼리
+
+DROP TABLE IF EXISTS tb_dart_stock;
+DROP TABLE IF EXISTS tb_dart_director;
+DROP TABLE IF EXISTS tb_dart_employee;
+DROP TABLE IF EXISTS tb_pollution;
+DROP TABLE IF EXISTS tb_edp;
+DROP TABLE IF EXISTS tb_waste_status;
+DROP TABLE IF EXISTS tb_green_gas_status;
+DROP TABLE IF EXISTS tb_nps;
+DROP TABLE IF EXISTS tb_esg_rating;
+DROP TABLE IF EXISTS tb_green_status;
+DROP TABLE IF EXISTS tb_report;
+DROP TABLE IF EXISTS tb_company_news;
+DROP TABLE IF EXISTS tb_company;
+DROP TABLE IF EXISTS tb_news;
+DROP TABLE IF EXISTS tb_keyword;
+DROP TABLE IF EXISTS tb_category;
+
+
